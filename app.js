78f9a7742ff00f8d5395dc4daa12eaf33e6ff3e2
rahulbadhe <39849178+rahulbadhe@users.noreply.github.com>
2021-03-16 17:12:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ var app = express();
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`server is running`);
+});
+
+//app.listen(3000);
